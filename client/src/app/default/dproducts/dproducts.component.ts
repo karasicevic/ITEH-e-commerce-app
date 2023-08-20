ae11be0ef258daf32e12de01a4ecc2dd43b8118b
Ivan Jovanovic <ij20190045@student.fon.bg.ac.rs>
2023-08-20 12:27:47 +0200

    napravljeni admin i default stranice i rute

---
+++ client/src/app/default/dproducts/dproducts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dproducts',
+  templateUrl: './dproducts.component.html',
+  styleUrls: ['./dproducts.component.scss']
+})
+export class DproductsComponent {
+
+}
